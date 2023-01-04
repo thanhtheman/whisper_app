ff2ff17732501041fdb2ea94fd17a6922d5b558e
thanh quach <thanhb.quach@gmail.com>
2023-01-03 23:02:37 -0500

    animation styling

--- whisper/profiles/forms.py
+++ whisper/profiles/forms.py
@@ -5,12 +5,9 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm (UserCreationForm):
     class Meta:
         model = User
-        fields =['first_name', 'username', 'email', 'password1', 'password2']
-        labels = {
-            'first_name': 'name'
-        }
+        fields =['username', 'email', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
-            field.widget.attrs.update({'class': 'new_class'})
+            field.widget.attrs.update({'class': 'bg-light rounded-pill form-control px-0.5 text-dark', 'style': 'font-size: 0.75em;'})
--- whisper/static/js/message.js
+++ whisper/static/js/message.js
@@ -1,13 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    hljs.highlightAll();
-  });
+document.addEventListener('DOMContentLoaded', () => {});
+
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   hljs.highlightAll();
+  // });
   
   
-  let alertWrapper = document.querySelector('.alert')
-  let alertClose = document.querySelector('.alert__close')
+  let alertWrapper = document.getElementById('alert')
+  let alertClose = document.getElementById('alert__close')
   
   if (alertWrapper) {
-    alertClose.addEventListener('click', () =>
-      alertWrapper.style.display = 'none'
-    )
+    alertClose.addEventListener('click', () => {
+      alertWrapper.style.display = 'none';
+      alertClose.style.display = 'none';
+    })
   }
